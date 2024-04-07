df6a53154d4f15e81fed607b41018be40b9ea518
Samaan Kamal <samaankamal72@gmail.com>
2024-04-07 19:39:19 +0200

    added Organizer class implementation

--- src/main/java/com/example/Task/Management/Entity/Organizer.java
+++ src/main/java/com/example/Task/Management/Entity/Organizer.java
@@ -1,4 +1,72 @@
 package com.example.Task.Management.Entity;
 
+import jakarta.persistence.*;
+import org.checkerframework.checker.units.qual.C;
+
+@Entity
+@Table(name = "Organizer")
 public class Organizer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "organizer_id")
+    private Integer organizerId;
+
+    @OneToOne(mappedBy = "organizer")
+    private Event event;
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "display_name")
+    private String displayName;
+    @Column(name = "self")
+    private boolean self;
+
+    public Organizer() {
+    }
+
+    public Organizer(String email, String displayName, boolean self) {
+        this.email = email;
+        this.displayName = displayName;
+        this.self = self;
+    }
+
+    public Integer getOrganizerId() {
+        return organizerId;
+    }
+
+    public void setOrganizerId(Integer organizerId) {
+        this.organizerId = organizerId;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isSelf() {
+        return self;
+    }
+
+    public void setSelf(boolean self) {
+        this.self = self;
+    }
 }
