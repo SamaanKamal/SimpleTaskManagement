b74882387af1262cf0a301cef2420134a1d04747
Samaan Kamal <samaankamal72@gmail.com>
2024-04-07 20:01:46 +0200

    added proprties and secured sensitive data needed for database

--- src/main/java/com/example/Task/Management/TaskManagementApplication.java
+++ src/main/java/com/example/Task/Management/TaskManagementApplication.java
@@ -2,8 +2,10 @@ package com.example.Task.Management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:src/environment.properties")
 public class TaskManagementApplication {
 
 	public static void main(String[] args) {
