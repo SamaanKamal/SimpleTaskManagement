e2591408d436dc7d54a45dbb9690666405a467c7
Samaan Kamal <samaankamal72@gmail.com>
2024-04-07 19:12:56 +0200

    added attachment class implementation

--- src/main/java/com/example/Task/Management/Entity/Attachments.java
+++ src/main/java/com/example/Task/Management/Entity/Attachments.java
@@ -1,4 +1,81 @@
 package com.example.Task.Management.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "attachments")
 public class Attachments {
+    @Column(name = "fileUrl")
+    private String fileUrl;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "mimetype")
+    private String mimetype;
+    @Column(name = "iconLink")
+    private String iconLink;
+    @Column(name = "fileId")
+    private String fileId;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    public Attachments() {
+    }
+
+    public Attachments(String fileUrl, String title, String mimetype, String iconLink, String fileId) {
+        this.fileUrl = fileUrl;
+        this.title = title;
+        this.mimetype = mimetype;
+        this.iconLink = iconLink;
+        this.fileId = fileId;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMimetype() {
+        return mimetype;
+    }
+
+    public void setMimetype(String mimetype) {
+        this.mimetype = mimetype;
+    }
+
+    public String getIconLink() {
+        return iconLink;
+    }
+
+    public void setIconLink(String iconLink) {
+        this.iconLink = iconLink;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
