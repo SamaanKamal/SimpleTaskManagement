a1fec6d55ab5f05703edcf8088cf1665611a74a5
Samaan Kamal <samaankamal72@gmail.com>
2024-04-07 19:35:24 +0200

    added Creator class implementation

--- src/main/java/com/example/Task/Management/Entity/Creator.java
+++ src/main/java/com/example/Task/Management/Entity/Creator.java
@@ -1,4 +1,71 @@
 package com.example.Task.Management.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Creator")
 public class Creator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "creator_id")
+    private Integer creatorId;
+
+    @OneToOne(mappedBy = "creator")
+    private Event event;
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "display_name")
+    private String displayName;
+    @Column(name = "self")
+    private boolean self;
+
+    public Creator() {
+    }
+
+    public Creator(String email, String displayName, boolean self) {
+        this.email = email;
+        this.displayName = displayName;
+        this.self = self;
+    }
+
+    public Integer getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Integer creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isSelf() {
+        return self;
+    }
+
+    public void setSelf(boolean self) {
+        this.self = self;
+    }
 }
