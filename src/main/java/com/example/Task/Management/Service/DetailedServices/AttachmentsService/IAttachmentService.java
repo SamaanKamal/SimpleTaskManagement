6167ae1b72bb1beaa62af2e5233b83c0a811d349
Samaan Kamal <samaankamal72@gmail.com>
2024-04-09 00:12:42 +0200

    added attachment interface for the attachment service

---
+++ src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/IAttachmentService.java
@@ -0,0 +1,9 @@
+package com.example.Task.Management.Service.DetailedServices.AttachmentsService;
+
+import com.example.Task.Management.Entity.Attachment;
+
+import java.util.List;
+
+public interface IAttachmentService {
+
+}
