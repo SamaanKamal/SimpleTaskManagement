c58bfa760164379aa6618aa61ef8b3f89bc56c0c
Samaan Kamal <samaankamal72@gmail.com>
2024-04-08 20:09:52 +0200

    updated Confgiuration class and solved the error

--- src/main/java/com/example/Task/Management/Configuration/GoogleCalendarConfiguration.java
+++ src/main/java/com/example/Task/Management/Configuration/GoogleCalendarConfiguration.java
@@ -1,6 +1,10 @@
 package com.example.Task.Management.Configuration;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 @Configuration
@@ -19,14 +24,15 @@ public class GoogleCalendarConfiguration {
     @Value("${spring.application.name}")
     private  String appName;
     @Bean
-    public Calendar calendarService() throws IOException {
-        // Load the service account credentials from JSON key file
+    public Calendar calendarService() throws IOException, GeneralSecurityException {
+
         GoogleCredential credential = GoogleCredential.fromStream(
                          new FileInputStream(serviceAccountKeyFile))
                 .createScoped(Collections.singleton(CalendarScopes.CALENDAR_READONLY));
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
-        // Build and return a Google Calendar service
-        return new Calendar.Builder(credential.getTransport(), credential.getJsonFactory(), null)
+        return new Calendar.Builder(httpTransport, jsonFactory,credential)
                 .setApplicationName(appName)
                 .build();
     }
