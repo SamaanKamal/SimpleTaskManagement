dbddcb6d641c7bccdb8496a45a4ddeb2b3390e9e
Samaan Kamal <samaankamal72@gmail.com>
2024-04-08 18:32:35 +0200

    added configuration for the api

--- src/main/java/com/example/Task/Management/Configuration/GoogleCalendarConfiguration.java
+++ src/main/java/com/example/Task/Management/Configuration/GoogleCalendarConfiguration.java
@@ -1,8 +1,33 @@
 package com.example.Task.Management.Configuration;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collections;
 
 @Configuration
 public class GoogleCalendarConfiguration {
+    @Value("${google.calendar.service-account-key-file}")
+    private String serviceAccountKeyFile;
 
+    @Value("${spring.application.name}")
+    private  String appName;
+    @Bean
+    public Calendar calendarService() throws IOException {
+        // Load the service account credentials from JSON key file
+        GoogleCredential credential = GoogleCredential.fromStream(
+                         new FileInputStream(serviceAccountKeyFile))
+                .createScoped(Collections.singleton(CalendarScopes.CALENDAR_READONLY));
+
+        // Build and return a Google Calendar service
+        return new Calendar.Builder(credential.getTransport(), credential.getJsonFactory(), null)
+                .setApplicationName(appName)
+                .build();
+    }
 }
