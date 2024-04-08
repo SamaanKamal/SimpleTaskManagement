40065c3c084aa47b8cb2e1756f76aae69b1621d0
Samaan Kamal <samaankamal72@gmail.com>
2024-04-09 00:20:20 +0200

    added the crud methods into the interface

--- src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/IAttachmentService.java
+++ src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/IAttachmentService.java
@@ -1,9 +1,14 @@
 package com.example.Task.Management.Service.DetailedServices.AttachmentsService;
 
 import com.example.Task.Management.Entity.Attachment;
+import com.example.Task.Management.Entity.Event;
 
 import java.util.List;
 
 public interface IAttachmentService {
+    List<Attachment> getAllAttachments();
+    String createAttachment(Attachment attachment);
+    String updateAttachment(Integer attachmentId, Attachment attachment);
+    String deleteAttachment(Integer attachmentId);
 
 }
