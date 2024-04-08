fa5abe701d6b2f59bde4a6be61eaefc6e04d2934
Samaan Kamal <samaankamal72@gmail.com>
2024-04-09 01:34:12 +0200

    implemented needed methods in service

--- src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/AttachmentService.java
+++ src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/AttachmentService.java
@@ -1,4 +1,68 @@
 package com.example.Task.Management.Service.DetailedServices.AttachmentsService;
 
-public class AttachmentService {
+import com.example.Task.Management.Entity.Attachment;
+import com.example.Task.Management.Helpers.AttachmentHelper.AttachmentRequest;
+import com.example.Task.Management.Repository.AttachmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AttachmentService implements IAttachmentService {
+    @Autowired
+    private AttachmentRepository attachmentRepository;
+
+    @Override
+    public List<Attachment> getAllAttachments() {
+        return attachmentRepository.findAll();
+    }
+
+    @Override
+    public Attachment getAttachment(Integer attachmentId) {
+        return attachmentRepository.findById(attachmentId).orElseThrow(()->
+                new RuntimeException("Attachment not found with id: " + attachmentId));
+    }
+
+    @Override
+    public String createAttachment(AttachmentRequest attachmentRequest) {
+        Attachment attachment = new Attachment();
+        attachment.setFileId(attachmentRequest.getFileId());
+        attachment.setFileUrl(attachmentRequest.getFileUrl());
+        attachment.setMimetype(attachmentRequest.getMimetype());
+        attachment.setTitle(attachmentRequest.getTitle());
+        attachment.setIconLink(attachmentRequest.getIconLink());
+        var savedAttachment =attachmentRepository.save(attachment);
+        if(savedAttachment==null){
+            return null;
+        }
+        return "Attachment Created Successfully";
+    }
+
+    @Override
+    public String updateAttachment(Integer attachmentId, AttachmentRequest attachmentRequest) {
+        Attachment attachment = attachmentRepository.findById(attachmentId).orElseThrow(()->
+                new RuntimeException("Attachment not found with id: " + attachmentId));
+        attachment.setFileId(attachmentRequest.getFileId());
+        attachment.setFileUrl(attachmentRequest.getFileUrl());
+        attachment.setMimetype(attachmentRequest.getMimetype());
+        attachment.setTitle(attachmentRequest.getTitle());
+        attachment.setIconLink(attachmentRequest.getIconLink());
+        var updatedAttachment =attachmentRepository.save(attachment);
+        if(updatedAttachment==null){
+            return null;
+        }
+        return "Attachment Updated Successfully";
+    }
+
+    @Override
+    public String deleteAttachment(Integer attachmentId) {
+        Attachment attachment =attachmentRepository.findById(attachmentId).orElseThrow(() ->
+                new RuntimeException("Attachment not found with id: " + attachmentId));;
+        if(attachment!=null){
+            attachmentRepository.delete(attachment);
+            return "Attachment Deleted Successfully";
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/IAttachmentService.java
+++ src/main/java/com/example/Task/Management/Service/DetailedServices/AttachmentsService/IAttachmentService.java
@@ -2,13 +2,16 @@ package com.example.Task.Management.Service.DetailedServices.AttachmentsService;
 
 import com.example.Task.Management.Entity.Attachment;
 import com.example.Task.Management.Entity.Event;
+import com.example.Task.Management.Helpers.AttachmentHelper.AttachmentRequest;
 
 import java.util.List;
 
 public interface IAttachmentService {
     List<Attachment> getAllAttachments();
-    String createAttachment(Attachment attachment);
-    String updateAttachment(Integer attachmentId, Attachment attachment);
+
+    Attachment getAttachment(Integer attachmentId);
+    String createAttachment(AttachmentRequest attachmentRequest);
+    String updateAttachment(Integer attachmentId, AttachmentRequest attachmentRequest);
     String deleteAttachment(Integer attachmentId);
 
 
