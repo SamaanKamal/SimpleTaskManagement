209a057b00bcb840560108fa91da6b2216532662
Samaan Kamal <samaankamal72@gmail.com>
2024-04-08 23:23:00 +0200

    added cahce class for events using in memory hashmap

---
+++ src/main/java/com/example/Task/Management/Cache/EventCache.java
@@ -0,0 +1,35 @@
+package com.example.Task.Management.Cache;
+
+import com.example.Task.Management.Entity.Event;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EventCache {
+    private final Map<Integer, Event> cache = new HashMap<>();
+
+    public void addEvent(Integer eventId, Event event) {
+        cache.put(eventId, event);
+    }
+
+    public void updateEvent(Integer eventId, Event event) {
+        if (cache.containsKey(eventId)) {
+            cache.put(eventId, event);
+        }
+    }
+
+    public void deleteEvent(Integer eventId) {
+        cache.remove(eventId);
+    }
+
+    public Event getEvent(Integer eventId) {
+        return cache.get(eventId);
+    }
+
+    public Collection<Event> getAllEvents() {
+        return cache.values();
+    }
+}
