760ed4d3b7506b9f2631de36cfdd95510c2be5f0
Samaan Kamal <samaankamal72@gmail.com>
2024-04-09 14:43:37 +0200

    added creator service class to implement crud methods

---
+++ src/main/java/com/example/Task/Management/Service/DetailedServices/CreatorService/CreatorService.java
@@ -0,0 +1,53 @@
+package com.example.Task.Management.Service.DetailedServices.CreatorService;
+
+import com.example.Task.Management.Entity.Creator;
+import com.example.Task.Management.Helpers.CreatorHelper.CreatorRequest;
+import com.example.Task.Management.Repository.CreatorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CreatorService implements  ICreatorService {
+
+    @Autowired
+    private CreatorRepository creatorRepository;
+
+    @Override
+    public List<Creator> getAllCreators() {
+        return creatorRepository.findAll();
+    }
+
+    @Override
+    public Creator getCreator(Integer creatorId) {
+        return creatorRepository.findById(creatorId).orElseThrow(()->
+                new RuntimeException("Creator not found with id: " + creatorId));
+    }
+
+    @Override
+    public void createCreator(CreatorRequest creatorRequest) {
+        Creator creator = new Creator();
+        creator.setEmail(creatorRequest.getEmail());
+        creator.setDisplayName(creatorRequest.getDisplayName());
+        creator.setSelf(creatorRequest.isSelf());
+        creatorRepository.save(creator);
+    }
+
+    @Override
+    public void updateCreator(Integer creatorId, CreatorRequest creatorRequest) {
+        Creator creator =creatorRepository.findById(creatorId).orElseThrow(()->
+                new RuntimeException("Creator not found with id: " + creatorId));
+        creator.setEmail(creatorRequest.getEmail());
+        creator.setDisplayName(creatorRequest.getDisplayName());
+        creator.setSelf(creatorRequest.isSelf());
+        creatorRepository.save(creator);
+    }
+
+    @Override
+    public void deleteCreator(Integer creatorId) {
+        Creator creator =creatorRepository.findById(creatorId).orElseThrow(()->
+                new RuntimeException("Creator not found with id: " + creatorId));
+        creatorRepository.delete(creator);
+    }
+}
--- src/main/java/com/example/Task/Management/Service/DetailedServices/CreatorService/ICreatorService.java
+++ src/main/java/com/example/Task/Management/Service/DetailedServices/CreatorService/ICreatorService.java
@@ -3,13 +3,14 @@ package com.example.Task.Management.Service.DetailedServices.CreatorService;
 
 
 import com.example.Task.Management.Entity.Creator;
+import com.example.Task.Management.Helpers.CreatorHelper.CreatorRequest;
 
 import java.util.List;
 
 public interface ICreatorService {
     List<Creator> getAllCreators();
     Creator getCreator(Integer creatorId);
-    void createCreator();
-    void updateCreator(Integer creatorId);
+    void createCreator(CreatorRequest creatorRequest);
+    void updateCreator(Integer creatorId,CreatorRequest creatorRequest);
     void deleteCreator(Integer creatorId);
 }
