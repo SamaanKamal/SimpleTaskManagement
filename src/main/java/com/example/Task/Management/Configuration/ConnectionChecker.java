8dc1553d2ad502e0cab89dc33de28055e25097c6
Samaan Kamal <samaankamal72@gmail.com>
2024-04-13 20:58:11 +0200

    edited the checking method because the first one was returning true every time

--- src/main/java/com/example/Task/Management/Configuration/ConnectionChecker.java
+++ src/main/java/com/example/Task/Management/Configuration/ConnectionChecker.java
@@ -2,14 +2,20 @@ package com.example.Task.Management.Configuration;
 
 import org.springframework.stereotype.Component;
 
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.URL;
 
 @Component
 public class ConnectionChecker {
     public boolean isInternetConnected() {
         try {
-            InetAddress address = InetAddress.getLoopbackAddress();
-            return address.isReachable(1000);
+            URL url = new URL("https://www.google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.setConnectTimeout(1000); // Timeout in milliseconds
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
         } catch (Exception e) {
             return false;
         }
